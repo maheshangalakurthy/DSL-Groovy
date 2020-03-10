02cf1d54f13c6b685b38a78d8057ae1e129aa7e9
maheshangalakurthy <39906066+maheshangalakurthy@users.noreply.github.com>
2020-03-10 11:18:13 +0530

    defect fix

--- maheshtest.groovy
+++ maheshtest.groovy
@@ -4,7 +4,7 @@ pipelineJob('maheshtest') {
 	parameters {
         booleanParam('FLAG', true,'select true')
         choiceParam('where to run', ['Docker (default)', 'AIX', 'Both'],'selectone')
-		stringParam('hostname','0494','selected host to run)
+		stringParam('hostname','0494','selected host to run')
     }
 	
     definition {
